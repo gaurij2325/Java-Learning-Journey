e9628840786904d4f686a3596c724205d025d8fc
gaurij2325 <168436911+gaurij2325@users.noreply.github.com>
2026-04-21 19:14:10 +0530

    Update miniATM.java

--- Java-Fundamentals/miniATM/miniATM.java
+++ Java-Fundamentals/miniATM/miniATM.java
@@ -1,1 +1,64 @@
+import java.util.Scanner;
+
+public class miniATM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int initialAmt = 1000;
+        int choice;
+        do {
+
+            System.out.println("""
+                        ----MENU----
+                        1.Check balance
+                        2. Deposit
+                        3. Withdraw
+                        4. Exit
+                    """);
+
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.println("your balance is: " + initialAmt);
+                    break;
+
+                case 2:
+                    System.out.println("enter the amount to be deposited: ");
+                    int deposit = sc.nextInt();
+                    // sc.nextLine();
+                    initialAmt += deposit;
+                    System.out.println("the total balance after depositing is " + initialAmt);
+                    break;
+
+                case 3:
+                    System.out.println("enter the amount to be withdrawn: ");
+                    int withdraw = sc.nextInt();
+                    // sc.nextLine();
+                    if (withdraw < initialAmt) {
+                        System.out.println("processing withdrawal..");
+                        initialAmt -= withdraw;
+                        System.out.println("the total balance after withdrawal is " + initialAmt);
+
+                    } else {
+                        System.out.println("the value to be withdrawn exceeds the total balance");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Thank you!");
+                    break;
+
+                default:
+                    System.out.println("invalid choice");
+
+            }
+        } while (choice != 4);
+        sc.close();
+
+    }
+
+}
 
